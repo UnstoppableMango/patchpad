b65e0f43a626d123d3ea5ee3fbe63f14536c4047
UnstoppableMango <erik.rasmussen@unmango.dev>
2026-06-21 22:02:58 -0500

    Nix stuff

--- flake.nix
+++ flake.nix
@@ -1,30 +1,48 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.inputs.systems.follows = "systems";
+    };
   };
 
-  outputs = { self, nixpkgs, fenix }:
-    let
-      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forAllSystems = nixpkgs.lib.genAttrs systems;
-    in
-    {
-      devShells = forAllSystems (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          toolchain = fenix.packages.${system}.stable.toolchain;
-        in
-        {
-          default = pkgs.mkShell {
-            packages = [
-              toolchain
-              pkgs.nix
-            ];
-          };
-        });
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+
+      imports = with inputs; [
+        treefmt-nix.flakeModule
+      ];
+
+      perSystem = { pkgs, inputs', ... }: {
+        packages.default = pkgs.callPackage ./nix {
+          toolchain = inputs'.fenix.packages.stable.toolchain;
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            inputs'.fenix.packages.stable.toolchain
+            nix
+            nixfmt
+          ];
+        };
+
+        treefmt = {
+          projectRootFile = "flake.nix";
+          programs.rustfmt.enable = true;
+          programs.nixfmt.enable = true;
+        };
+      };
     };
 }
